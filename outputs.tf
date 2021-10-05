4228dce948fe3b57eea2d3dcaca37ecb6bb6c8fc
Michael Minichino <mminichino@mac.com>
2021-10-05 19:32:54 +0000

    Bug fixes

--- outputs.tf
+++ outputs.tf
@@ -2,31 +2,45 @@ output lab-id {
     value = "lab-${random_id.labid.hex}"
 }
 
-output "db-node-ips" {
+output "db-node-public" {
   value = [
     for instance in aws_instance.couchbase_nodes:
     instance.public_ip
   ]
 }
 
-output "gen-node-ips" {
+output "gen-node-public" {
   value = [
     for instance in aws_instance.generator_nodes:
     instance.public_ip
+  ]
+}
+
+output "inventory_db" {
+  value = [
+    for instance in aws_instance.couchbase_nodes:
+    instance.private_ip
+  ]
+}
+
+output "inventory_gen" {
+  value = [
+    for instance in aws_instance.generator_nodes:
+    instance.private_ip
   ]
 }
 
 output "db-node-names" {
   value = [
     for instance in aws_instance.couchbase_nodes:
-    lookup(instance.tags, "Name")
+    "${lookup(instance.tags, "Name")}.${var.domain_name}"
   ]
 }
 
 output "gen-node-names" {
   value = [
     for instance in aws_instance.generator_nodes:
-    lookup(instance.tags, "Name")
+    "${lookup(instance.tags, "Name")}.${var.domain_name}"
   ]
 }
 
